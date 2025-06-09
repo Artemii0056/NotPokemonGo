9739ca0c3aada2dadcf20ff7a8e899a371cb3df7
Arseniy Kuvatow <seniakuv99@gmail.com>
2025-06-09 03:26:31 +0300

    fix files hirerachy. added VContainer. Added RaindowFolder Package

---
+++ Source/NotPokemonGo/Assets/Scripts/Elements/Configs/ElementConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Infrastructure.Scripts.Elements
+{
+	[CreateAssetMenu(fileName = nameof(ElementConfig), menuName = "Config/" + nameof(ElementConfig))]
+	public class ElementConfig : ScriptableObject
+	{
+		public class ElementInfo
+		{
+			
+		}
+	}
+}
---
+++ Source/NotPokemonGo/Assets/Scripts/Elements/ElementType.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Scripts.Elements
+{
+	public enum ElementType
+	{
+		Unknown = 0,
+		
+		Fire = 1,
+		Water = 0,
+		Earth = 0,
+		Air = 7,
+	}
+}
