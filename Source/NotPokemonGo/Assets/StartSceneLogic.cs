eb95ecd0b1f3bcf02e406e1dd8e2acab4122a24e
Artemii <aza_art@mail.ru>
2025-07-06 22:15:16 +0500

    lil fix

---
+++ Source/NotPokemonGo/Assets/StartSceneLogic.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.Design;
+using Services.AssetManagement;
+using Services.StaticDataServices;
+
+namespace DefaultNamespace
+{
+    public class StartSceneLogic
+    {
+        private IStaticDataService _staticDataService;
+        private IResourceLoader _resourceLoader;
+
+        public StartSceneLogic(IStaticDataService staticDataService)
+        {
+            _staticDataService = staticDataService;
+        }
+    }
+}
