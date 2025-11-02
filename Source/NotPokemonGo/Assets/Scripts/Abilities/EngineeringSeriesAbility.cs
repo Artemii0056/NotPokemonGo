0f4b9288f420aa5c8f415e0967ff11a46275fad4
Artemii <aza_art@mail.ru>
2025-11-03 00:48:38 +0500

    updated bennet skill

--- Source/NotPokemonGo/Assets/Scripts/Abilities/EngineeringSeriesAbility.cs
+++ Source/NotPokemonGo/Assets/Scripts/Abilities/EngineeringSeriesAbility.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Cameras;
+using Cinemachine;
 using Infrastructure;
 using QTESystem;
 using Services;
@@ -10,32 +11,27 @@ using Units;
 using Units.AnimationControllers;
 using UnityEngine;
 using DG.Tweening;
+using Object = UnityEngine.Object;
 
 namespace Abilities
 {
     public class EngineeringSeriesAbility
     {
-        private Unit _source;
-        private Unit _target;
-
-        private UnitAnimatorTrigger _unitAnimationTrigger;
-        private UnitAnimatorController _unitAnimatorController;
+        private readonly Unit _source;
+        private readonly Unit _target;
+        private readonly UnitAnimatorController _animatorController;
+        private readonly UnitAnimatorTrigger _animatorTrigger;
         private readonly ICoroutineRunner _coroutineRunner;
         private readonly IQteService _qteService;
+        private readonly List<AbilityPart> _parts;
+        private readonly Vector3 _startPosition;
+        private readonly CinemachineBrain _cinemachineBrain;
 
-        private int _currentPhaseIndex;
-
-        private List<AbilityPart> _parts;
-        private Vector3 _startPosition;
-
+        private Coroutine _currentRoutine;
+        private bool _lastQteSuccess;
+        
         private bool _animationPlaying;
 
-        private Coroutine _coroutine;
-
-        private bool _tapCompleted;
-        private bool _backCompleted;
-        private bool _forwardCompleted;
-        
         public event Action Finished;
 
         public EngineeringSeriesAbility(
@@ -45,182 +41,176 @@ namespace Abilities
             ICoroutineRunner coroutineRunner,
             IQteService qteService)
         {
-            _target = target;
             _source = source;
-
+            _target = target;
             _coroutineRunner = coroutineRunner;
             _qteService = qteService;
 
-            _unitAnimatorController = source.UnitAnimatorController;
-            _unitAnimationTrigger = source.AnimatorTrigger;
-
+            _animatorController = source.UnitAnimatorController;
+            _animatorTrigger = source.AnimatorTrigger;
             _parts = abilityProvider.AbilityModel.Parts;
             _startPosition = source.transform.position;
+            _cinemachineBrain = Object.FindObjectOfType<CinemachineBrain>(); //TODO Вот эту херню исправить 
         }
 
         public void Play()
         {
-            _currentPhaseIndex = 0;
-
-            _coroutine = _coroutineRunner.StartCoroutine(HandlePhase(_parts[_currentPhaseIndex].AbilityPhases));
+            _currentRoutine = _coroutineRunner.StartCoroutine(ExecuteAllParts());
         }
 
-        private IEnumerator HandlePhase(List<AbilityPhase> phases)
+        private IEnumerator ExecuteAllParts()
         {
-            foreach (AbilityPhase abilityPhase in phases)
+            for (int partIndex = 0; partIndex < _parts.Count; partIndex++)
             {
-                _unitAnimationTrigger.SetPhase(abilityPhase);
-                _unitAnimatorController.Play(abilityPhase.AnimationCashName);
+                var part = _parts[partIndex];
 
-                if (abilityPhase.CameraActionType != CameraActionType.None)
-                    HandleCamera(_source, abilityPhase.CameraActionType);
-
-                if (abilityPhase.QteType != QteType.Unknown)
+                for (int phaseIndex = 0; phaseIndex < part.AbilityPhases.Count; phaseIndex++)
                 {
-                    if (abilityPhase.QteType == QteType.TapToButton)
+                    var phase = part.AbilityPhases[phaseIndex];
+
+                    // 🔸 пример: пропустить фазы после неудачного QTE
+                    if (!_lastQteSuccess && phase.QteType == QteType.SliderForward)
                     {
-                        Time.timeScale = 0.1f;
-                        _qteService.Completed += OnQteTapToButtonCompleted;
-                        _qteService.Start(abilityPhase.QteType);
+                        Debug.Log($"Фаза {phaseIndex} пропущена из-за неудачного QTE");
+                        continue;
                     }
-                    else if (abilityPhase.QteType == QteType.SliderBack)
-                    {
-                        if (_tapCompleted)
-                        {
-                            Time.timeScale = 0.25f; // 
-                            _qteService.Completed += OnQteSliderBackCompleted;
-                            _qteService.Start(abilityPhase.QteType);
-                        }
-                    }
-                    else if (abilityPhase.QteType == QteType.SliderForward)
-                    {
-                        if (_backCompleted)
-                        {
-                            Time.timeScale = 0.1f;
-                            _qteService.Completed += OnQteSliderForwardCompleted;
-                            _qteService.Start(abilityPhase.QteType);
-                        }
-                    }
-                }
 
-                switch (abilityPhase.PhaseType)
-                {
-                    
-                        // _animationPlaying = true;
-                        // _unitAnimatorController.Finished += AnimationFinished;
-                        //
-                        // yield return new WaitWhile(() => _animationPlaying);
-                        //
-                        // _unitAnimatorController.Finished -= AnimationFinished;
-                        // break;
-
-                    case PhaseType.IsMovementPhase: 
-                        yield return MoveUnit(_source, CalculateTargetPosition(_source.transform.position, _target.transform.position));
-                        break;
-
-                    case PhaseType.IsReturnPhase:
-                        yield return MoveUnit(_source, _startPosition);
-                        break;
-
-                    case PhaseType.IsMelee:
-                    case PhaseType.Default:
-                        _animationPlaying = true;
-                        _unitAnimatorController.Finished += AnimationFinished;
-
-                        yield return new WaitWhile(() => _animationPlaying);
-
-                        _unitAnimatorController.Finished -= AnimationFinished;
-                        break;
+                    yield return ExecutePhase(phase);
                 }
             }
 
-            HandleEndPhase();
+            FinishAbility();
         }
 
-        private void HandleCamera(Unit source, CameraActionType actionType)
+        private IEnumerator ExecutePhase(AbilityPhase phase)
         {
-            if (actionType == CameraActionType.FocusOnSource)
-                source.virtualCamera.enabled = true;
-            else if (actionType == CameraActionType.MoveBack)
-                source.virtualCamera.enabled = false;
-        }
+            _animatorTrigger.SetPhase(phase);
+            _animatorController.Play(phase.AnimationCashName);
 
-        private void OnQteSliderBackCompleted(bool state)
-        {
-            Debug.Log("OnQteSliderBackCompleted " + state);
+            HandleCamera(phase.CameraActionType);
 
-            Time.timeScale = 1f;
-            _backCompleted = state;
-            _qteService.Completed -= OnQteSliderBackCompleted;
-        }
+            if (phase.QteType != QteType.Unknown)
+                yield return RunQtePhase(phase.QteType);
 
-        private void OnQteSliderForwardCompleted(bool state)
-        {
-            Time.timeScale = 1f;
-
-            _source.virtualCamera.enabled = false;
-            Debug.Log("ПРОЙДЕНО!");
-            _qteService.Completed -= OnQteSliderForwardCompleted;
-        }
-
-        private void OnQteTapToButtonCompleted(bool state)
-        {
-            Time.timeScale = 1f;
-            Debug.Log("OnQteTapToButtonCompleted " + state);
-
-            _tapCompleted = state;
-
-            _qteService.Completed -= OnQteTapToButtonCompleted;
-
-            // if (state == false)
-            //     _currentPhaseIndex++;
-        }
-
-        private void HandleEndPhase()
-        {
-            _currentPhaseIndex++;
-
-            if (_currentPhaseIndex >= _parts.Count)
+            switch (phase.PhaseType)
             {
-                _unitAnimatorController.Play(Constants.BaseAnimations.Idle);
-                Finished?.Invoke();
-                return;
+                case PhaseType.IsMovementPhase:
+                    yield return MoveUnit(_source, CalculateTargetPosition(_source.transform.position, _target.transform.position));
+                    break;
+
+                case PhaseType.IsReturnPhase:
+                    yield return MoveUnit(_source, _startPosition);
+                    break;
+
+                default:
+                    yield return WaitForAnimation();
+                    break;
+            }
+        }
+
+        private IEnumerator RunQtePhase(QteType qteType)
+        {
+            bool completed = false;
+            bool result = false;
+
+            void OnCompleted(bool success)
+            {
+                completed = true;
+                result = success;
+                _qteService.Completed -= OnCompleted;
             }
 
-            if (_coroutine != null)
-                _coroutineRunner.StopCoroutine(_coroutine);
+            _qteService.Completed += OnCompleted;
+            SetTimeScaleForQte(qteType);
+            _qteService.Start(qteType);
 
-            _coroutine = _coroutineRunner.StartCoroutine(HandlePhase(_parts[_currentPhaseIndex].AbilityPhases));
+            yield return new WaitUntil(() => completed);
+
+            Time.timeScale = 1f;
+            _lastQteSuccess = result;
+
+            Debug.Log($"QTE {qteType} завершено. Успех: {_lastQteSuccess}");
         }
 
-        private void AnimationFinished() =>
-            _animationPlaying = false;
+        private void SetTimeScaleForQte(QteType type)
+        {
+            Time.timeScale = type switch
+            {
+                QteType.TapToButton => 0.1f,
+                QteType.SliderBack => 0.1f,
+                QteType.SliderForward => 0.1f,
+                _ => 1f
+            };
+        }
 
-        private IEnumerator MoveUnit(Unit unit, Vector3 targetPosition)
+        private void HandleCamera(CameraActionType actionType)
+        {
+            switch (actionType)
+            {
+                case CameraActionType.FocusOnSource:
+                    _source.virtualCamera.enabled = true;
+                    _coroutineRunner.StartCoroutine(WaitForBlendEnd());
+                    break;
+
+                case CameraActionType.MoveBack:
+                    _source.virtualCamera.enabled = false;
+                    break;
+            }
+        }
+
+        private IEnumerator WaitForBlendEnd()
+        {
+            yield return new WaitForEndOfFrame();
+            
+            while (_cinemachineBrain.ActiveBlend != null)
+                yield return null;
+            
+            FinishAnimation();
+        }
+
+        private IEnumerator WaitForAnimation()
+        {
+            _animationPlaying = true; 
+            
+            void OnFinished() => FinishAnimation();
+
+            _animatorController.Finished += OnFinished;
+            yield return new WaitWhile(() => _animationPlaying);
+            _animatorController.Finished -= OnFinished;
+        }
+
+        private IEnumerator MoveUnit(Unit unit, Vector3 target)
         {
             float liftDelay = 0.6f;
             int jumpPower = 2;
-            var totalDuration = _unitAnimatorController.GetAnimationLength();
+            var duration = _animatorController.GetAnimationLength();
 
             yield return new WaitForSeconds(liftDelay);
 
-            float moveDuration = totalDuration - liftDelay;
+            float moveDuration = duration - liftDelay;
             unit.transform.DOKill();
 
             Tween jumpTween = unit.transform
-                .DOJump(targetPosition, jumpPower, 1, moveDuration)
+                .DOJump(target, jumpPower, 1, moveDuration)
                 .SetEase(Ease.InQuad);
 
             yield return jumpTween.WaitForCompletion();
         }
-        
-        private Vector3 CalculateTargetPosition( Vector3 startPosition,Vector3 targetPosition)
+
+        private Vector3 CalculateTargetPosition(Vector3 start, Vector3 target)
         {
             float stopDistance = 1.5f;
-            
-            Vector3 direction = (targetPosition - startPosition).normalized;
-            
-            return targetPosition - direction * stopDistance;
+            Vector3 direction = (target - start).normalized;
+            return target - direction * stopDistance;
         }
+
+        private void FinishAbility()
+        {
+            _animatorController.Play(Constants.BaseAnimations.Idle);
+            Finished?.Invoke();
+        }
+        
+        private void FinishAnimation() => 
+            _animationPlaying = false;
     }
 }
