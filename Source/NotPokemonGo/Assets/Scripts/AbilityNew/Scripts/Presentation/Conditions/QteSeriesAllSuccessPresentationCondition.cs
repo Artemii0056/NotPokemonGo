c455936918bd3da5248eba91d072393b8f21b9cb
Artemii <aza_art@mail.ru>
2026-04-04 12:40:59 +0500

    update

---
+++ Source/NotPokemonGo/Assets/Scripts/AbilityNew/Scripts/Presentation/Conditions/QteSeriesAllSuccessPresentationCondition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AbilityNew.Scripts.Presentation.Conditions
+{
+    [Serializable]
+    public sealed class QteSeriesAllSuccessPresentationCondition : PresentationCondition
+    {
+        public override bool Evaluate(AbilityPresentationContext context) =>
+            context.QteSeriesResult != null && context.QteSeriesResult.AllSuccessOrBetter;
+    }
+}
---
+++ Source/NotPokemonGo/Assets/Scripts/AbilityNew/Scripts/Presentation/Conditions/QteSeriesHasFailPresentationCondition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AbilityNew.Scripts.Presentation.Conditions
+{
+    [Serializable]
+    public sealed class QteSeriesHasFailPresentationCondition : PresentationCondition
+    {
+        public override bool Evaluate(AbilityPresentationContext context) =>
+            context.QteSeriesResult != null && context.QteSeriesResult.HasFail;
+    }
+}
